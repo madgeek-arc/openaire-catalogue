87c1069b6ba37d7be978079a245c498894a4e0ce
Konstantinos Spyrou <spyroukon@gmail.com>
2023-01-12 18:06:22 +0200

    add filters only when non null

--- src/main/java/gr/madgik/catalogue/openaire/resource/repository/ServiceRepository.java
+++ src/main/java/gr/madgik/catalogue/openaire/resource/repository/ServiceRepository.java
@@ -23,8 +23,12 @@ public class ServiceRepository extends RegistryCoreRepository<ServiceBundle, Str
     @Override
     public ServiceBundle get(String s, String catalogue) {
         FacetFilter filter = new FacetFilter();
-        filter.addFilter("resource_internal_id", s);
-        filter.addFilter("catalogue_id", catalogue);
+        if (s != null) {
+            filter.addFilter("resource_internal_id", s);
+        }
+        if (catalogue != null) {
+            filter.addFilter("catalogue_id", catalogue);
+        }
         Paging<ServiceBundle> results = get(filter);
         if (results.getResults() != null && !results.getResults().isEmpty()) {
             return results.getResults().get(0);
