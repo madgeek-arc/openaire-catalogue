f034d03620f146fb766279a4193acfa7fcef83a6
Konstantinos Spyrou <spyroukon@gmail.com>
2023-04-03 18:01:48 +0300

    configure server url to avoid cors error

--- src/main/java/gr/madgik/catalogue/openaire/config/OpenApiConfig.java
+++ src/main/java/gr/madgik/catalogue/openaire/config/OpenApiConfig.java
@@ -1,14 +1,43 @@
 package gr.madgik.catalogue.openaire.config;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
-import org.springframework.web.filter.ForwardedHeaderFilter;
+import org.springframework.context.annotation.Configuration;
 
-@Component
+import java.util.List;
+
+@Configuration
 public class OpenApiConfig {
 
+    @Value("${swagger.host}")
+    String host;
+
     @Bean
-    ForwardedHeaderFilter forwardedHeaderFilter() {
-        return new ForwardedHeaderFilter();
+    public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
+        return new OpenAPI()
+                .addServersItem(new Server().url(host))
+//                .components(
+//                        new Components().addSecuritySchemes(
+//                                securitySchemeName,
+//                                new SecurityScheme()
+//                                        .type(SecurityScheme.Type.HTTP)
+//                                        .scheme("bearer")
+//                                        .bearerFormat("JWT")
+//                        )
+//                )
+//                .security(List.of(new SecurityRequirement().addList(securitySchemeName)))
+                .info(
+                        new Info()
+                                .title("OpenAIRE Service Catalogue API")
+                                .version("1")
+                                .license(new License().name("Apache 2.0").url("")));
     }
 }
