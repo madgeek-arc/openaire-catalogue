5aebe39a1f9e638d7f5049974e35c703ade05138
Konstantinos Spyrou <spyroukon@gmail.com>
2024-01-22 17:49:15 +0200

    complete logout

--- src/main/java/gr/madgik/catalogue/openaire/config/SecurityConfig.java
+++ src/main/java/gr/madgik/catalogue/openaire/config/SecurityConfig.java
@@ -6,13 +6,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
+import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority;
 import org.springframework.security.oauth2.core.user.OAuth2UserAuthority;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 import java.util.HashSet;
 import java.util.Map;
@@ -24,11 +28,14 @@ public class SecurityConfig {
     private static final Logger logger = LoggerFactory.getLogger(SecurityConfig.class);
 
     private final AuthenticationSuccessHandler authSuccessHandler;
+    private final ClientRegistrationRepository clientRegistrationRepository;
     private final ApplicationProperties applicationProperties;
 
     public SecurityConfig(AuthenticationSuccessHandler authSuccessHandler,
+                          ClientRegistrationRepository clientRegistrationRepository,
                           ApplicationProperties applicationProperties) {
         this.authSuccessHandler = authSuccessHandler;
+        this.clientRegistrationRepository = clientRegistrationRepository;
         this.applicationProperties = applicationProperties;
     }
 
@@ -44,16 +51,30 @@ public class SecurityConfig {
                         .antMatchers(HttpMethod.GET, "/vocabularies/**").permitAll()
                         .regexMatchers("/dump/.*", "/restore/", "/resources.*", "/version.*", "/items.*", "/resourceType.*", "/search.*", "/logs.*", "/forms.*", "/vocabularies.*").hasAnyAuthority("ADMIN")
                         .anyRequest().permitAll())
-                .oauth2Login()
-                .successHandler(authSuccessHandler)
-                .and()
-                .logout().logoutSuccessUrl(applicationProperties.getLogoutRedirect())
-                .deleteCookies().clearAuthentication(true).invalidateHttpSession(true)
-                .and()
-                .cors()
-                .and()
-                .csrf().disable();
+
+                .oauth2Login(oauth2login -> oauth2login
+                        .successHandler(authSuccessHandler))
+
+                .logout(logout -> logout
+                        .logoutSuccessHandler(oidcLogoutSuccessHandler())
+                        .deleteCookies()
+                        .clearAuthentication(true)
+                        .invalidateHttpSession(true))
+
+                .cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable);
         return http.build();
+    }
+
+    @Bean
+    public LogoutSuccessHandler oidcLogoutSuccessHandler() {
+        OidcClientInitiatedLogoutSuccessHandler oidcLogoutSuccessHandler =
+                new OidcClientInitiatedLogoutSuccessHandler(
+                        this.clientRegistrationRepository);
+
+        oidcLogoutSuccessHandler.setPostLogoutRedirectUri(applicationProperties.getLogoutRedirect());
+
+        return oidcLogoutSuccessHandler;
     }
 
     @Bean
