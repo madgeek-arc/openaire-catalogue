b91e2ea5d667d42323675249150b2bdd55c5088c
mzouros <michael.zouros@outlook.com>
2024-02-23 17:01:59 +0200

    Removed extras fromn Datasource model. Minor bug fix on onboard() method for Services/Datasources

--- src/main/java/gr/madgik/catalogue/openaire/domain/Datasource.java
+++ src/main/java/gr/madgik/catalogue/openaire/domain/Datasource.java
@@ -5,52 +5,30 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import eu.einfracentral.domain.*;
 import org.json.simple.JSONObject;
 
-import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 import java.net.URL;
-import java.util.Date;
 import java.util.List;
 
 @XmlType(name = "service")
 @XmlRootElement(namespace = "http://openaire.eu")
 public class Datasource extends eu.einfracentral.domain.Datasource {
 
-    @XmlElement
-    private JSONObject extras;
-
     public Datasource() {
     }
 
     public Datasource(String id, String serviceId, String catalogueId, URL submissionPolicyURL, URL preservationPolicyURL, Boolean versionControl, List<PersistentIdentitySystem> persistentIdentitySystems, String jurisdiction, String datasourceClassification, List<String> researchEntityTypes, Boolean thematic, List<ResearchProductLicensing> researchProductLicensings, List<String> researchProductAccessPolicies, ResearchProductMetadataLicensing researchProductMetadataLicensing, List<String> researchProductMetadataAccessPolicies, Boolean harvestable, JSONObject extras) {
         super(id, serviceId, catalogueId, submissionPolicyURL, preservationPolicyURL, versionControl, persistentIdentitySystems, jurisdiction, datasourceClassification, researchEntityTypes, thematic, researchProductLicensings, researchProductAccessPolicies, researchProductMetadataLicensing, researchProductMetadataAccessPolicies, harvestable);
-        this.extras = extras;
-    }
-
-    public JSONObject getExtras() {
-        return extras;
-    }
-
-    public void setExtras(JSONObject extras) {
-        this.extras = extras;
     }
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Datasource)) return false;
-        if (!super.equals(o)) return false;
-
-        Datasource that = (Datasource) o;
-
-        return getExtras() != null ? getExtras().equals(that.getExtras()) : that.getExtras() == null;
+        return super.equals(o);
     }
 
     @Override
     public int hashCode() {
-        int result = super.hashCode();
-        result = 31 * result + (getExtras() != null ? getExtras().hashCode() : 0);
-        return result;
+        return super.hashCode();
     }
 
     @Override
--- src/main/java/gr/madgik/catalogue/openaire/domain/DatasourceBundle.java
+++ src/main/java/gr/madgik/catalogue/openaire/domain/DatasourceBundle.java
@@ -1,9 +1,7 @@
 package gr.madgik.catalogue.openaire.domain;
 
-import eu.einfracentral.annotation.FieldValidation;
 import eu.einfracentral.domain.Bundle;
 import eu.einfracentral.domain.Metadata;
-import eu.einfracentral.domain.ResourceExtras;
 
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
@@ -15,10 +13,6 @@ public class DatasourceBundle extends Bundle<Datasource> {
 
     @XmlElement
     private String status;
-
-    @XmlElement
-    @FieldValidation(nullable = true)
-    private ResourceExtras resourceExtras;
 
     public DatasourceBundle() {
         // No arg constructor
@@ -60,14 +54,6 @@ public class DatasourceBundle extends Bundle<Datasource> {
 
     public void setStatus(String status) {
         this.status = status;
-    }
-
-    public ResourceExtras getResourceExtras() {
-        return resourceExtras;
-    }
-
-    public void setResourceExtras(ResourceExtras resourceExtras) {
-        this.resourceExtras = resourceExtras;
     }
 
     @Override
